4c14a76e687149b0525bc481e7c4dcbf7d99081b
Noamico123 <Noam.Mishaeli@gmail.com>
2021-01-02 18:28:20 +0200

    all 4 btns working, for 'move' btn -> left click for copy and right click for paste and remove from source

--- widgets/buttons_view.py
+++ widgets/buttons_view.py
@@ -18,16 +18,19 @@ class ButtonsView:
         self.src_path = ""
         self.dst_path = ""
         self.file_name = ""
-        self.copy_btn.pack(side=tk.LEFT, padx=(ButtonsView.MARGIN_X * 2, ButtonsView.MARGIN_X))
+        self.copy_btn.pack(side=tk.LEFT, padx=(ButtonsView.MARGIN_X * 2, ButtonsView.MARGIN_X),
+                           pady=ButtonsView.MARGIN_X)
         self.paste_btn = tk.Button(self.frame, text="Paste", font="Arial 9 bold")
-        self.paste_btn.pack(side=tk.LEFT, padx=(ButtonsView.MARGIN_X, ButtonsView.MARGIN_X))
+        self.paste_btn.pack(side=tk.LEFT, padx=(ButtonsView.MARGIN_X, ButtonsView.MARGIN_X), pady=ButtonsView.MARGIN_X)
         self.move_btn = tk.Button(self.frame, text="Move", font="Arial 9 bold")
-        self.move_btn.pack(side=tk.LEFT, padx=(ButtonsView.MARGIN_X, ButtonsView.MARGIN_X))
+        self.move_btn.pack(side=tk.LEFT, padx=(ButtonsView.MARGIN_X, ButtonsView.MARGIN_X), pady=ButtonsView.MARGIN_X)
         self.delete_btn = tk.Button(self.frame, text="Delete", font="Arial 9 bold")
-        self.delete_btn.pack(side=tk.LEFT, padx=(ButtonsView.MARGIN_X, ButtonsView.MARGIN_X))
-
+        self.delete_btn.pack(side=tk.LEFT, padx=(ButtonsView.MARGIN_X, ButtonsView.MARGIN_X), pady=ButtonsView.MARGIN_X)
         self.copy_btn.bind("<Button-1>", self.handle_copy_event)
         self.paste_btn.bind("<Button-1>", self.handle_paste_event)
+        self.move_btn.bind("<Button-1>", self.handle_copy_event)
+        self.move_btn.bind("<Button-3>", self.handle_move_event)
+        self.delete_btn.bind("<Button-1>", self.handle_delete_event)
         self.frame.grid(row=2, column=0,
                         sticky=tk.NSEW,
                         padx=ButtonsView.MARGIN_X, pady=ButtonsView.MARGIN_Y)
@@ -37,8 +40,21 @@ class ButtonsView:
         print(self.src_path)
 
     def handle_paste_event(self, event):
+        self.cpy_src_dst()
+        self.controller.update_all_views(self.controller.view.navbar.path.get())
+
+    def handle_move_event(self, event):
+        self.cpy_src_dst()
+        os.remove(self.src_path)
+        self.controller.update_all_views(self.controller.view.navbar.path.get())
+
+    def handle_delete_event(self, event):
+        self.src_path = self.controller.view.navbar.path.get() + "\\" + self.file_name
+        os.remove(self.src_path)
+        self.controller.update_all_views(self.controller.view.navbar.path.get())
+
+    def cpy_src_dst(self):
         without_extra_slash = os.path.normpath(self.src_path)
         file_to_copy = os.path.basename(without_extra_slash)
         self.dst_path = self.controller.view.navbar.path.get() + "\\" + file_to_copy
-        print(self.dst_path)
         shutil.copyfile(self.src_path, self.dst_path)
